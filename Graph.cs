393c3e429468bab62dfe7a5f84718de5a8875685
meogol <avs99.98@gmail.com>
2019-04-05 21:58:57 +0300

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,190 @@
+
+using System.Collections.Generic;
+using Egar.Focus.DealModule.Fields;
+using Egar.Focus.Framework.Types;
+
+namespace ConsoleApp1
+{
+		
+	public class EntityBaseTest_Graph : GraphBase
+	{	
+		(ConsoleApp2.EntityBaseTest)Entity;
+
+		public EntityBaseTest_Graph(FieldsEntity entity/*, EntityExtentionBase extention*/) :base(entity/*, extention*/)
+		{
+			this.Entity= (EntityBaseTest)entity;
+		}
+		
+		#region define order
+
+		protected Dictionary<string, List<CalcFieldInfo>> CreateCalcOrder()
+		{
+			var calcOrder = new Dictionary<string, List<CalcFieldInfo>>()
+			{
+							#region input order
+                {"", new List<CalcFieldInfo>()
+					{
+										new CalcFieldInfo(fild3, new List<FieldBase>(){}),
+	
+					}
+				},
+				#endregion input order
+						
+				
+
+				
+				#region fild3
+                {"fild3", new List<CalcFieldInfo>()
+					{
+				
+					}
+				},
+				#endregion fild3
+						
+				
+
+				
+			
+			};//new Dictionary
+		 }
+	return calcOrder;
+		}
+	}
+		#endregion //define order
+	
+	public class EntityTest_Graph : GraphBase
+	{	
+		(ConsoleApp2.EntityTest)Entity;
+
+		public EntityTest_Graph(FieldsEntity entity/*, EntityExtentionBase extention*/) :base(entity/*, extention*/)
+		{
+			this.Entity= (EntityTest)entity;
+		}
+		
+		#region define order
+
+		protected Dictionary<string, List<CalcFieldInfo>> CreateCalcOrder()
+		{
+			var calcOrder = new Dictionary<string, List<CalcFieldInfo>>()
+			{
+							#region input order
+                {"", new List<CalcFieldInfo>()
+					{
+										new CalcFieldInfo(fild1, new List<FieldBase>(){}),
+	
+					}
+				},
+				#endregion input order
+						
+				
+
+				
+				#region fild1
+                {"fild1", new List<CalcFieldInfo>()
+					{
+				
+					}
+				},
+				#endregion fild1
+						
+				
+
+				
+			
+			};//new Dictionary
+		 }
+	return calcOrder;
+		}
+	}
+		#endregion //define order
+	
+	public class Entity_Graph : GraphBase
+	{	
+		(ConsoleApp2.Entity)Entity;
+
+		public Entity_Graph(FieldsEntity entity/*, EntityExtentionBase extention*/) :base(entity/*, extention*/)
+		{
+			this.Entity= (Entity)entity;
+		}
+		
+		#region define order
+
+		protected Dictionary<string, List<CalcFieldInfo>> CreateCalcOrder()
+		{
+			var calcOrder = new Dictionary<string, List<CalcFieldInfo>>()
+			{
+							#region input order
+                {"", new List<CalcFieldInfo>()
+					{
+										new CalcFieldInfo(fild1, new List<FieldBase>(){}),
+							new CalcFieldInfo(fild3, new List<FieldBase>(){}),
+							new CalcFieldInfo(fild3_1, new List<FieldBase>(){}),
+							new CalcFieldInfo(testFild, new List<FieldBase>(){}),
+	
+					}
+				},
+				#endregion input order
+						
+				
+
+				
+				#region fild1
+                {"fild1", new List<CalcFieldInfo>()
+					{
+										new CalcFieldInfo(fild3, new List<FieldBase>(){}),
+							new CalcFieldInfo(fild3_1, new List<FieldBase>(){}),
+							new CalcFieldInfo(testFild, new List<FieldBase>(){}),
+	
+					}
+				},
+				#endregion fild1
+						
+				
+
+				
+				#region fild3
+                {"fild3", new List<CalcFieldInfo>()
+					{
+										new CalcFieldInfo(fild3_1, new List<FieldBase>(){}),
+							new CalcFieldInfo(testFild, new List<FieldBase>(){}),
+	
+					}
+				},
+				#endregion fild3
+						
+				
+
+				
+				#region testFild
+                {"testFild", new List<CalcFieldInfo>()
+					{
+				
+					}
+				},
+				#endregion testFild
+						
+				
+
+				
+				#region fild3_1
+                {"fild3_1", new List<CalcFieldInfo>()
+					{
+				
+					}
+				},
+				#endregion fild3_1
+						
+				
+
+				
+			
+			};//new Dictionary
+		 }
+	return calcOrder;
+		}
+	}
+		#endregion //define order
+				
+				
+	}
+}
